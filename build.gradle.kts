a13a1c8fd337212a69224f83592bf6292ce60f56
Mambo <kdevkr@gmail.com>
2025-03-26 22:08:44 +0900

    chore: add freefair Lombok plugin to build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
+    id("io.freefair.lombok") version "8.13.1"
 }
 
 group = "kr.kdev"
@@ -27,9 +28,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation(libs.bundles.simplejavamail)
-    compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
