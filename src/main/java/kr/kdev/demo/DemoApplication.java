8770e157fa3b64dbef5e23e14ea27592f268ef73
Mambo <kdevkr@gmail.com>
2025-03-28 21:27:32 +0900

    feat: implement email queue with retry mechanism and scheduling

--- src/main/java/kr/kdev/demo/DemoApplication.java
+++ src/main/java/kr/kdev/demo/DemoApplication.java
@@ -3,14 +3,16 @@ package kr.kdev.demo;
 import org.simplejavamail.springsupport.SimpleJavaMailSpringSupport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Import(SimpleJavaMailSpringSupport.class)
+@EnableScheduling
+@Import({MailSenderAutoConfiguration.class, SimpleJavaMailSpringSupport.class})
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
---
+++ src/main/java/kr/kdev/demo/SimpleMailQueue.java
@@ -0,0 +1,63 @@
+package kr.kdev.demo;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Queues;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.simplejavamail.MailException;
+import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.mailer.Mailer;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SimpleMailQueue {
+    private static final int MAX_RETRY_COUNT = 3;
+    private final Mailer mailer;
+    private final BlockingQueue<Email> queue = Queues.newLinkedBlockingDeque();
+    private final Map<Integer, Integer> retryCount = Maps.newConcurrentMap();
+
+    public void produce(Email email) {
+        queue.add(email);
+    }
+
+    @Scheduled(fixedRateString = "PT1S")
+    public void consume() {
+        if (queue.isEmpty()) {
+            return;
+        }
+
+        List<Email> emails = new LinkedList<>();
+        queue.drainTo(emails, 10);
+
+        for (Email email : emails) {
+            try {
+                mailer.sendMail(email);
+                log.info("Message-ID: {}", email.getId());
+            } catch (MailException e) {
+                log.error("Failed to send email: {}", e.getMessage());
+                handleEmailSendFailure(email);
+            }
+        }
+    }
+
+    private void handleEmailSendFailure(Email email) {
+        int emailHash = email.hashCode();
+        int currentRetryCount = retryCount.getOrDefault(emailHash, 0) + 1;
+        if (currentRetryCount > MAX_RETRY_COUNT) {
+            retryCount.remove(emailHash);
+            return;
+        }
+
+        log.info("Retry send email {}/{}", currentRetryCount, MAX_RETRY_COUNT);
+        retryCount.put(emailHash, currentRetryCount);
+        queue.add(email);
+    }
+}
